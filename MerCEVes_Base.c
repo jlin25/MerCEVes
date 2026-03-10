d67747fd7912916edb137fa0cc55550ef2ae27dc
jlin25 <136258177+jlin25@users.noreply.github.com>
2026-03-10 17:57:51 -0400

    jetson stuff

--- MerCEVes_Base.c
+++ MerCEVes_Base.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <math.h>
 #include "hardware/adc.h"
 #include "hardware/pwm.h"
+#include "hardware/spi.h"
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 #include "stepper.h"
@@ -25,7 +27,12 @@
 #define RPM_PIN_R 28
 
 //spi
-#define SPI_PORT spi0;
+#define SPI_CLK 6
+#define SPI_TX 7
+#define SPI_RX 8
+#define SPI_CS 9
+
+#define M_PI 3.14159265358979323846
 
 const int steps_per_rev = 1600;
 const int margin_of_error = 20;
@@ -37,14 +44,64 @@ volatile int current_step_angle;
 volatile int pulse_count_L = 0;
 volatile int pulse_count_R = 0;
 
-const int magnets_per_wheel = 68;
-const int radius = .86;
+volatile int requested_speed = 0;
+volatile int requested_angle = 1000;
 
-void rpm_interrupt_L(uint gpio, uint32_t events) {
-    pulse_count_L++;
-}
-void rpm_interrupt_R(uint gpio, uint32_t events) {
-    pulse_count_R++;
+volatile double speed_L = 0;
+volatile double speed_R = 0;
+
+const int magnets_per_wheel = 68;
+const double radius = .86;
+
+void gpio_interrupt(uint gpio, uint32_t events) {
+    switch(gpio) {
+        case RPM_PIN_L:
+            if(events == GPIO_IRQ_EDGE_RISE) pulse_count_L++;
+            break;
+        case RPM_PIN_R:
+            if(events == GPIO_IRQ_EDGE_RISE) pulse_count_R++;
+            break;
+        case SPI_CS:
+            if(events != GPIO_IRQ_EDGE_FALL) break;
+            uint8_t buffer[18];
+            buffer[0] = 0xBB;
+            for(int i = 1; i < 9; i++) {
+                buffer[i] = (uint8_t)((uint64_t)(speed_L)>>(8 * (i-1))) & 0xFF;
+            }
+            for(int i = 9; i < 17; i++) {
+                buffer[i] = (uint8_t)((uint64_t)(speed_R)>>(8 * (i-9))) & 0xFF;
+            }
+            uint8_t checksum = 0;
+            for(int i = 0; i < 17; i++) {
+                checksum ^= buffer[i];
+            }
+            buffer[17] = checksum;
+            uint8_t buffer_out[18];
+            spi_write_read_blocking(spi0, buffer, buffer_out, 18);
+            
+            checksum = 0;
+            for(int i = 0; i < 17; i++) {
+                checksum ^= buffer_out[i];
+            }
+            if(buffer_out[17] != checksum) {
+                printf("checksum failed :(");
+                break;
+            }
+            
+            uint64_t speed;
+            for(int i = 1; i < 9; i++) {
+                speed |= buffer_out[i] << (8 * (i-1));
+            }
+            double speed_d = (double)speed;
+            requested_speed = (int)round(speed_d);
+
+            uint64_t angle_int;
+            for(int i = 9; i < 17; i++) {
+                angle_int |= buffer_out[i] << (8 * (i-9));
+            }
+            double angle_d = (double)speed;
+            requested_angle = (int)(1000 * ((angle_d + (M_PI/2))/(M_PI)));
+    }
 }
 
 static inline int pollADC() {
@@ -54,9 +111,12 @@ static inline int pollADC() {
 
 void core1_entry() {
     while (true) {
-        int adc_value = pollADC();
+        //int adc_value = pollADC();
+        int adc_value = requested_angle;
         int error = current_step_angle - adc_value;
 
+        uint dc_slice_num = pwm_gpio_to_slice_num(DC_PWM_PIN);
+        pwm_set_chan_level(dc_slice_num, PWM_CHAN_A, requested_speed);
         gpio_put(DIR_PIN, error > 0 ? 0 : 1);
 
         if (abs(error) > margin_of_error) {
@@ -87,8 +147,8 @@ int main() {
     uint dc_slice_num = pwm_gpio_to_slice_num(DC_PWM_PIN);
 
     pwm_set_enabled(dc_slice_num, true);
-    pwm_set_wrap(dc_slice_num, 1000); // 16-bit resolution
-    pwm_set_chan_level(dc_slice_num, PWM_CHAN_A, 150); // 50% duty cycle
+    pwm_set_wrap(dc_slice_num, 2048); // 16-bit resolution
+    pwm_set_chan_level(dc_slice_num, PWM_CHAN_A, requested_speed); 
 
     gpio_init(M1INA_PIN);
     gpio_set_dir(M1INA_PIN, GPIO_OUT);
@@ -104,31 +164,32 @@ int main() {
     //rpm
     gpio_init(RPM_PIN_L);
     gpio_set_dir(RPM_PIN_L, GPIO_IN);
-    gpio_set_irq_enabled_with_callback(RPM_PIN_L, GPIO_IRQ_EDGE_RISE, true, &rpm_interrupt_L);
+    gpio_set_irq_enabled_with_callback(RPM_PIN_L, GPIO_IRQ_EDGE_RISE, true, &gpio_interrupt);
 
     gpio_init(RPM_PIN_R);
     gpio_set_dir(RPM_PIN_R, GPIO_IN);
-    gpio_set_irq_enabled_with_callback(RPM_PIN_R, GPIO_IRQ_EDGE_RISE, true, &rpm_interrupt_R);
+    gpio_set_irq_enabled_with_callback(RPM_PIN_R, GPIO_IRQ_EDGE_RISE, true, &gpio_interrupt);
+
+    gpio_init(SPI_CS);
+    gpio_set_dir(SPI_CS, GPIO_IN);
+    gpio_set_irq_enabled_with_callback(SPI_CS, GPIO_IRQ_EDGE_FALL, true, &gpio_interrupt);
+
+    spi_init(spi0, 1000 * 1000);
+    spi_set_slave(spi0, true);
+    gpio_set_function(SPI_RX, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_CLK, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_TX, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_CS, GPIO_FUNC_SPI);
 
     while (true) {
         sleep_ms(100);
-        double rpm_L = (pulse_count_L* 2 * 2 * 3.141592) / magnets_per_wheel; // Convert to RPM
-        printf("RPM (rad/s): %f\n", rpm_L);
+        speed_L = (pulse_count_L* 2 * 2 * 3.141592) / magnets_per_wheel; // Convert to RPM
+        printf("RPM (rad/s): %f\n", speed_L);
         pulse_count_L = 0; // Reset pulse count for the next interval
 
-        double rpm_R = (pulse_count_R* 2 * 2 * 3.141592) / magnets_per_wheel; // Convert to RPM
-        printf("RPM (rad/s): %f\n", rpm_R);
+        speed_R = (pulse_count_R* 2 * 2 * 3.141592) / magnets_per_wheel; // Convert to RPM
+        printf("RPM (rad/s): %f\n", speed_R);
         pulse_count_R = 0; // Reset pulse count for the next interval
-
-        // get data from spi
-        uint8_t rx_data[17] = {0};
-        // calculate checksum
-        uint8_t checksum = 0;
-        for (int i = 0; i < 16; i++) {
-            checksum ^= rx_data[i];
-        }
-        printf("Received checksum: %02X, Calculated checksum: %02X\n", rx_data[16], checksum);
-
     }
 
     
